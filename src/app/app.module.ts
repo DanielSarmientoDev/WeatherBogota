2af5f6801cc74a47393d21f43356c6e2e1576453
danielsarmientodev <danielsarmientoenterprise@gmail.com>
2020-12-22 20:50:19 -0500

    add Pipe convert Kelvin to Celcius and Kelvin to Farenheit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeModule } from './home/home.module';
+import { LayoutComponent } from './layout/layout.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    HttpClientModule,
+    SharedModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
